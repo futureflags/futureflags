6974e51956aa0aa3455440f6eaab8358d7ade772
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2020-07-18 23:56:00 -0300

    test: add more coverage on feature rate

--- pages/api/features/[name]/[userCode]/__tests__/index.test.ts
+++ pages/api/features/[name]/[userCode]/__tests__/index.test.ts
@@ -1,13 +1,7 @@
 import featuresFunction from '../index'
-import {
-  addFlag,
-  createFeature,
-  Feature,
-  isFeatureEnabled,
-} from '../../../../../../db'
+import { addFlag, createFeature, Feature } from '../../../../../../db'
 import { callApi, mountFeature } from '../../../../_testUtils'
 import faker from 'faker'
-import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript'
 
 describe('GET /feature/[name]/[userCode]', () => {
   test('feature is enabled', async () => {
@@ -47,7 +41,28 @@ describe('GET /feature/[name]/[userCode]', () => {
     expect(resUserB.jsonBody.enabled).toBeFalsy()
   })
 
-  test('feature is enabled only for a rate', async () => {
+  test('feature is enabled only for a 25% rate', async () => {
+    const userACode = faker.random.uuid()
+    const userBCode = faker.random.uuid()
+    const userCCode = faker.random.uuid()
+    const userDCode = faker.random.uuid()
+
+    const featureDoc = await createFeature(
+      mountFeature({ enabled: true, rate: 25 })
+    )
+
+    const { res: resUserA } = await callFlagGet(featureDoc.data.name, userACode)
+    const { res: resUserB } = await callFlagGet(featureDoc.data.name, userBCode)
+    const { res: resUserC } = await callFlagGet(featureDoc.data.name, userCCode)
+    const { res: resUserD } = await callFlagGet(featureDoc.data.name, userDCode)
+
+    expect(resUserA.jsonBody.enabled).toBeTruthy()
+    expect(resUserB.jsonBody.enabled).toBeFalsy()
+    expect(resUserC.jsonBody.enabled).toBeFalsy()
+    expect(resUserD.jsonBody.enabled).toBeFalsy()
+  })
+
+  test('feature is enabled only for a 50% rate', async () => {
     const userACode = faker.random.uuid()
     const userBCode = faker.random.uuid()
     const userCCode = faker.random.uuid()
@@ -64,6 +79,27 @@ describe('GET /feature/[name]/[userCode]', () => {
 
     expect(resUserA.jsonBody.enabled).toBeTruthy()
     expect(resUserB.jsonBody.enabled).toBeFalsy()
+    expect(resUserC.jsonBody.enabled).toBeTruthy()
+    expect(resUserD.jsonBody.enabled).toBeFalsy()
+  })
+
+  test('feature is enabled only for a 75% rate', async () => {
+    const userACode = faker.random.uuid()
+    const userBCode = faker.random.uuid()
+    const userCCode = faker.random.uuid()
+    const userDCode = faker.random.uuid()
+
+    const featureDoc = await createFeature(
+      mountFeature({ enabled: true, rate: 75 })
+    )
+
+    const { res: resUserA } = await callFlagGet(featureDoc.data.name, userACode)
+    const { res: resUserB } = await callFlagGet(featureDoc.data.name, userBCode)
+    const { res: resUserC } = await callFlagGet(featureDoc.data.name, userCCode)
+    const { res: resUserD } = await callFlagGet(featureDoc.data.name, userDCode)
+
+    expect(resUserA.jsonBody.enabled).toBeTruthy()
+    expect(resUserB.jsonBody.enabled).toBeTruthy()
     expect(resUserC.jsonBody.enabled).toBeTruthy()
     expect(resUserD.jsonBody.enabled).toBeFalsy()
   })
